9f664879c108591126c23384b82f3358fa7d0732
Alp Unal <45209073+lithellx@users.noreply.github.com>
2024-01-07 01:03:48 +0300

    Add files via upload

---
+++ Source/GameLibraryAutomation/BuyForm.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace GameLibraryAutomation
+{
+    partial class BuyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BuyForm));
+            this.payButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.balanceTBox = new System.Windows.Forms.TextBox();
+            this.costTBox = new System.Windows.Forms.TextBox();
+            this.nBalanceTBox = new System.Windows.Forms.TextBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.gameNameLabel = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // payButton
+            // 
+            this.payButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(169)))), ((int)(((byte)(3)))));
+            this.payButton.FlatAppearance.BorderSize = 0;
+            this.payButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.payButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.payButton.ForeColor = System.Drawing.Color.White;
+            this.payButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.payButton.Location = new System.Drawing.Point(20, 212);
+            this.payButton.Name = "payButton";
+            this.payButton.Size = new System.Drawing.Size(261, 32);
+            this.payButton.TabIndex = 20;
+            this.payButton.TabStop = false;
+            this.payButton.Text = "Satın Al";
+            this.payButton.UseVisualStyleBackColor = false;
+            this.payButton.Click += new System.EventHandler(this.payButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(58, 181);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(197, 17);
+            this.checkBox1.TabIndex = 22;
+            this.checkBox1.TabStop = false;
+            this.checkBox1.Text = "Sözleşme koşullarını kabul ediyorum.";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Silver;
+            this.label3.Location = new System.Drawing.Point(126, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 24);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "____";
+            // 
+            // balanceTBox
+            // 
+            this.balanceTBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.balanceTBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.balanceTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.balanceTBox.ForeColor = System.Drawing.Color.Silver;
+            this.balanceTBox.Location = new System.Drawing.Point(91, 79);
+            this.balanceTBox.Multiline = true;
+            this.balanceTBox.Name = "balanceTBox";
+            this.balanceTBox.ReadOnly = true;
+            this.balanceTBox.Size = new System.Drawing.Size(85, 27);
+            this.balanceTBox.TabIndex = 29;
+            this.balanceTBox.Text = "₺999";
+            this.balanceTBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // costTBox
+            // 
+            this.costTBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.costTBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.costTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.costTBox.ForeColor = System.Drawing.Color.Silver;
+            this.costTBox.Location = new System.Drawing.Point(91, 106);
+            this.costTBox.Multiline = true;
+            this.costTBox.Name = "costTBox";
+            this.costTBox.ReadOnly = true;
+            this.costTBox.Size = new System.Drawing.Size(85, 27);
+            this.costTBox.TabIndex = 30;
+            this.costTBox.Text = "-₺99";
+            this.costTBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // nBalanceTBox
+            // 
+            this.nBalanceTBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.nBalanceTBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nBalanceTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nBalanceTBox.ForeColor = System.Drawing.Color.LightGray;
+            this.nBalanceTBox.Location = new System.Drawing.Point(91, 139);
+            this.nBalanceTBox.Multiline = true;
+            this.nBalanceTBox.Name = "nBalanceTBox";
+            this.nBalanceTBox.ReadOnly = true;
+            this.nBalanceTBox.Size = new System.Drawing.Size(85, 27);
+            this.nBalanceTBox.TabIndex = 31;
+            this.nBalanceTBox.Text = "₺900";
+            this.nBalanceTBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.closeButton.Location = new System.Drawing.Point(20, 250);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(261, 24);
+            this.closeButton.TabIndex = 32;
+            this.closeButton.TabStop = false;
+            this.closeButton.Text = "Vazgeç";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // gameNameLabel
+            // 
+            this.gameNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gameNameLabel.ForeColor = System.Drawing.Color.White;
+            this.gameNameLabel.Location = new System.Drawing.Point(20, 18);
+            this.gameNameLabel.Name = "gameNameLabel";
+            this.gameNameLabel.Size = new System.Drawing.Size(261, 51);
+            this.gameNameLabel.TabIndex = 38;
+            this.gameNameLabel.Text = "GameName";
+            this.gameNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(169)))), ((int)(((byte)(3)))));
+            this.panel4.Location = new System.Drawing.Point(0, 297);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(300, 3);
+            this.panel4.TabIndex = 42;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(169)))), ((int)(((byte)(3)))));
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(300, 3);
+            this.panel3.TabIndex = 41;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(169)))), ((int)(((byte)(3)))));
+            this.panel2.Location = new System.Drawing.Point(297, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(3, 300);
+            this.panel2.TabIndex = 40;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(169)))), ((int)(((byte)(3)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(3, 300);
+            this.panel1.TabIndex = 39;
+            // 
+            // BuyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.nBalanceTBox);
+            this.Controls.Add(this.costTBox);
+            this.Controls.Add(this.balanceTBox);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.payButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gameNameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BuyForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YaltaCore - Ödeme";
+            this.Load += new System.EventHandler(this.BuyForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox balanceTBox;
+        public System.Windows.Forms.TextBox costTBox;
+        public System.Windows.Forms.TextBox nBalanceTBox;
+        private System.Windows.Forms.Button closeButton;
+        public System.Windows.Forms.Label gameNameLabel;
+        public System.Windows.Forms.Button payButton;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
